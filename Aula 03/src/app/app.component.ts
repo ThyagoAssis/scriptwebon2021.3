a3a1cc38deeac9ac73c2a65a65d521e4586eda61
Thyago <thyagoalmeidaprof@gmail.com>
2023-03-13 18:00:45 -0300

    Aula 04

--- Aula 03/src/app/app.component.ts
+++ Aula 03/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { throttleTime } from 'rxjs';
+
 
 //Decorator do angular
 @Component({
@@ -16,42 +16,9 @@ export class AppComponent {
   titleDois = "Event Binding";
   descriptionDois = "Captura uma informação ou um evento no arquivo .html e leva para o arquivo .ts" 
 
-  /*
-  //Diretiva - São recursos do Angular que permitem criar estrutras de repetição de decisao no HTML
-  //ngIf - Diretiva de decisão
-  //ngFor - Diretiva para repetição
-  */
-  
-  pessoa = {
-    nome: "Thyago Assis de Almeida",
-    idade: 15,
-    altura: 1.75,
-    aniversario: "15/02"
-  }
-  //carro = {} //Objeto
-  //mesa = [] //Array
-
-
-  //Array de Objetos
-  meusClientes = [
-    {nome: 'João', idade: 25},
-    {nome: 'Maria', idade: 15},
-    {nome: 'Marcos', idade: 30},
-    {nome: 'Mariana', idade: 10}
-
-  ]
-
-  //A diretiva ngFor é utilizada quando precisamos fazer um loop
-
-
-
-
-
   //Método do botão
   clicando(){
     alert("Clicou");
   }
-
-  
 
 }
--- Aula 03/src/app/app.module.ts
+++ Aula 03/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FotoComponent } from '../app/foto/foto.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FotoComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
