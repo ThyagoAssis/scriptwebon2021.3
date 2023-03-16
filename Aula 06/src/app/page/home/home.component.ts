9ba4d0992ec20a271388ca83b78a9cb968704ae6
Thyago <thyagoalmeidaprof@gmail.com>
2023-03-15 21:00:17 -0300

    Aula 06]

---
+++ Aula 06/src/app/page/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  imagemHome = "https://cdn.pixabay.com/photo/2020/04/20/14/47/car-5068532_960_720.png";
+}
---
+++ fotografia/src/app/page/fotografia/fotoformulario/fotoformulario.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fotoformulario',
+  templateUrl: './fotoformulario.component.html',
+  styleUrls: ['./fotoformulario.component.css']
+})
+export class FotoformularioComponent {
+
+}
